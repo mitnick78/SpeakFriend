6762634e0aee7a1b3e02c47e462679d9347def32
christophemilliere <christophemilliere93@gmail.com>
2022-08-29 22:55:21 +0200

    configuration Firebase

---
+++ SpeakFriends/Model/FirebaseHelper/FirebaseManager.swift
@@ -0,0 +1,28 @@
+//
+//  FirebaseManager.swift
+//  SpeakFriends
+//
+//  Created by christophe milliere on 29/08/2022.
+//
+
+import Foundation
+
+import Firebase
+
+class FirebaseManager {
+    static let shared = FirebaseManager()
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    //Auth
+    //SignIn
+    //Create
+    //Logout
+    
+    //CloudFirestore
+    //CreateUser
+    //CreateMessage
+    
+    //Storage
+}
---
+++ SpeakFriends/SpeakFriendsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SpeakFriendsApp.swift
+//  SpeakFriends
+//
+//  Created by christophe milliere on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeakFriendsApp: App {
+    let manager = FirebaseManager()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
